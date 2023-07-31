3f84988ad50b726ad477545e85efda06fa3d5c2f
ThoNguyen <hatigr.th@gmail.com>
2023-08-01 00:10:27 +0700

    update home page

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -1,8 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './product.scss'
+import { Link } from 'react-router-dom'
 
 const Product = () => {
+  const [isShow, setIsShow] = useState(false)
+  const newOrSale = 'new'
   return (
-    <div>Product</div>
+    <Link 
+      to={'/'} 
+      className='product'
+      onMouseEnter={(e) => {
+        e.stopPropagation();
+        setIsShow(true);
+      }}
+      onMouseLeave={(e) => {
+        e.stopPropagation();
+        setIsShow(false);
+      }}
+    >
+      <img className='img-product' src="https://vn-test-11.slatic.net/p/62c5feca63c68dc242e685d32d14850a.jpg" alt="" />
+      {isShow && (
+        <div className='product-action'>
+          <p className='add-product'>Mua ngay</p>
+          <p className='add-cart'>Thêm vào giỏ</p>
+        </div>
+      )}
+      <div className='product-detail'>
+        <p className='product-vendor'>Globber</p>
+        <p className='product-name'>Xe trượt scooter 3 bánh gấp gọn có bánh xe phát sáng GLOBBER PRIMO cho trẻ em từ độ tuổi dưới 2 năm tuổi nên sử dụng</p>
+        <div className='box-price'>
+          { newOrSale && (
+            <>
+              <p className={`${newOrSale === 'new' ? 'stick-new' : 'stick-sale'}`}>{newOrSale === 'new' ? 'New' : 'Sale'}</p>
+              <p className={`product-price ${newOrSale === 'new' ? 'new' : 'sale'}`}>2,888,777đ</p>
+            </>
+          )}
+        </div>
+      </div>
+    </Link>
   )
 }
 
--- src/pages/public/home/Home.jsx
+++ src/pages/public/home/Home.jsx
@@ -3,6 +3,7 @@ import SliderBanner from "../../../components/slider/SliderBanner";
 import './home.scss'
 import { categoryHome } from '../../../ultils/containts'
 import { Link } from "react-router-dom";
+import { Product } from "../../../components";
 
 const Home = () => {
   return (
@@ -23,7 +24,18 @@ const Home = () => {
       </div>
       <div className="discover">
         <h3 className="title-discover">Khám phá đồ chơi độc đáo</h3>
+        <Product/>
       </div>
+      <div className="video-outstanding">
+        <h2 className="title-video">Video sản phẩm nổi bật</h2>
+        <div className="list-video">
+          <iframe className="item-yt" width="25%" height="315" src="https://www.youtube.com/embed/L7xsdKi_TM8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+          <iframe className="item-yt" width="25%" height="315" src="https://www.youtube.com/embed/WZWyFE7bMoA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+          <iframe className="item-yt" width="25%" height="315" src="https://www.youtube.com/embed/qHwl_szNg4M" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        </div>
+      </div>
+      
+
     </div>
   )
 };
